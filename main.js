69548f766327c2336ae9e58c2e4d21bfb2750ffc
deniscelant <dedecelant123@gmail.com>
2025-02-18 17:13:31 -0300

    alpha

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
 import * as db from "/db.js"
 
-class barPanel{
-    constructor(name){
+class barPanel {
+    constructor(name) {
         this.name = name;
     }
-    createBar(name){
+    createBar(name) {
 
         const panel = document.createElement("div");
         panel.classList.add("barPanel");
         teste.appendChild(panel)
         panel.innerHTML = `
             <div id="bar">
-            <div id="progress">
-                <h3 id="project">${name}</h3>
-                <p id="percent">0%</p>
-                <p id="arrowDown">↓</p>
+                <div id="progress">
+                    <h3 id="project">${name}</h3>
+                    <p id="percent">0%</p>
+                    <p id="arrowDown">↓</p>
+                </div>
             </div>
+                <div class="modulesHub"></div>
         `
     }
 }
 
-class modules{
-    constructor(name){
+class modules {
+    constructor(name) {
         this.name = name;
     }
 
-    createTodo(){
-        teste.innerHTML = `<input type="checkbox">${userInput.value}.value</input>`
+    createTodo() {
+        const modulesHub = document.getElementsByClassName("modulesHub");
+        Array.from(modulesHub).forEach(e => e.innerHTML = `
+            <div id="inputArea">
+               <input id="check" type="checkbox"><input id="typeInput" type="text" placeholder="Digite aqui..."></input>
+           </div>
+           `) 
+        // const check = document.getElementById("check");
+        // const typeInput = document.getElementById("typeInput");
     }
 }
 
+let moduleUnit = "100px"
 const teste = document.getElementById("teste")
+
 const newbarPanel = new barPanel("Freelance")
+const newmodules = new modules();
 
 newbarPanel.createBar("Freelance");
+newmodules.createTodo();
+
+// barPanel.onclick = () =>{
+//     barPanel.classList.
+// }
