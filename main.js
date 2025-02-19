fbb58fcc62a5464534db7759c59cb00383715336
deniscelant <dedecelant123@gmail.com>
2025-02-19 14:18:46 -0300

    alpha

--- main.js
+++ main.js
@@ -1,53 +1,71 @@
 import * as db from "/db.js"
 
-class barPanel {
-    constructor(name) {
-        this.name = name;
+class panelBar {
+    constructor() {
     }
     createBar(name) {
-
         const panel = document.createElement("div");
-        panel.classList.add("barPanel");
-        teste.appendChild(panel)
-        panel.innerHTML = `
-            <div id="bar">
-                <div id="progress">
-                    <h3 id="project">${name}</h3>
-                    <p id="percent">0%</p>
-                    <p id="arrowDown">↓</p>
-                </div>
-            </div>
-                <div class="modulesHub"></div>
-        `
-    }
-}
 
-class modules {
-    constructor(name) {
-        this.name = name;
+        panel.classList.add("barPanel");
+        panel.id = "panelId"
+        teste.appendChild(panel)
+
+        panel.innerHTML = `
+        <div id="bar">
+            <div id="progress">
+                <h3 id="project">${name}</h3>
+                <p id="percent">0%</p>
+                <p id="arrowDown">↓</p>
+            </div>
+        </div>
+    `
+
+        const modulesID = document.createElement("div");
+        modulesID.classList.add("modulesHubHidden")
+        modulesID.id = "modulesId"
+        panel.appendChild(modulesID)
+
+        
+        const panelId = document.getElementById("panelId")
+        this.clickEvent(panelId, "openbarPanel", "barPanel")
+        this.clickEvent(modulesID, "modulesHubShow", "modulesHubHidden")
     }
 
     createTodo() {
-        const modulesHub = document.getElementsByClassName("modulesHub");
-        Array.from(modulesHub).forEach(e => e.innerHTML = `
+        const modulesHub = document.getElementsByClassName("modulesHubHidden");
+
+        modulesHub.innerHTML = `
             <div id="inputArea">
                <input id="check" type="checkbox"><input id="typeInput" type="text" placeholder="Digite aqui..."></input>
            </div>
-           `) 
-        // const check = document.getElementById("check");
-        // const typeInput = document.getElementById("typeInput");
+           `;
+
     }
+
+    clickEvent(element, firstClass, secondClass) {
+        element.onclick = () => {
+
+            element.classList.toggle(firstClass);
+            element.classList.toggle(secondClass);
+
+        }
+    }
+
+    todoButton() {
+        const todoButton = document.createElement("button")
+
+    }
+
+
 }
 
 let moduleUnit = "100px"
 const teste = document.getElementById("teste")
 
-const newbarPanel = new barPanel("Freelance")
-const newmodules = new modules();
+const newbarPanel = new panelBar()
 
 newbarPanel.createBar("Freelance");
-newmodules.createTodo();
+newbarPanel.createTodo();
 
-// barPanel.onclick = () =>{
-//     barPanel.classList.
-// }
+
+
